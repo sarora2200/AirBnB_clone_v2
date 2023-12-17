b1a14461e81ffa6d7e5c4117bc792769244cdb3a
sarora2200 <sarora2200@gmail.com>
2023-12-17 13:54:12 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-import models
+import os
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -23,40 +23,41 @@ class FileStorage:
         else:
             newdict = {}
             for key, value in self.__objects.items():
-                if type(value) == cls:
+                if isinstance(value, cls):
                     newdict[key] = value
             return newdict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
-        temp = {}
-        temp.update(FileStorage.__objects)
-        for key, val in temp.items():
-            temp[key] = val.to_dict()
+        temp = {key: val.to_dict() for key, val
+                in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
         try:
-            with open(FileStorage.__file_path, "r", encoding="UTF8") as fd:
-                for val in json.load(fd).values():
-                    class_name = val["__class__"]
-                    del val["__class__"]
-                    self.new(eval(class_name)(**val))
+            with open(os.path.abspath(FileStorage.__file_path),
+                      "r", encoding="UTF8") as fd:
+                data = json.load(fd)
+                for val in data.values():
+                    class_name = val.pop("__class__", None)
+                    if class_name and class_name in globals():
+                        cls = globals()[class_name]
+                        self.new(cls(**val))
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """Delete obj from __objects"""
         if obj:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects.pop(key, None)
 
     def close(self):
         """Deserializing the JSON file to objects"""
