9ede3ef17de827020afd37e6d04f18aa8b03f2f8
sarora2200 <sarora2200@gmail.com>
2023-12-17 13:11:55 -0800

    done

--- models/user.py
+++ models/user.py
@@ -7,17 +7,11 @@ from os import getenv
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes
-     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    """ This class defines a user by various attributes """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade="delete",backref="user")
-    reviews = relationship("Review", cascade="delete", backref="user")
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
