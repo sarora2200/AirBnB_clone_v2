4196f31a2f4b698b09e567373ab96f8db9f32e4c
sarora2200 <sarora2200@gmail.com>
2023-12-17 12:45:35 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -29,10 +29,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    kwargs['updated_at'] = datetime.strptime
-                    (kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                    kwargs['created_at'] = datetime.strptime
-                    (kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
             if "id" not in kwargs:
@@ -42,7 +39,6 @@ class BaseModel:
             if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
         else:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
