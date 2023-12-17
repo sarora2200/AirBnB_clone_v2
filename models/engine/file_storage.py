a9fea3f7952ababda9d797acdb97b83c7a8953bb
sarora2200 <sarora2200@gmail.com>
2023-12-17 11:25:16 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""File storage class for AirBnB"""
 import json
 from models.base_model import BaseModel
 from models.user import User
