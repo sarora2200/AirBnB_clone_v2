2ecfd83942427a8f08a2a0190ce3f836ca052d10
sarora2200 <sarora2200@gmail.com>
2023-12-17 10:58:37 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of objects"""
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if(partition[0] == cls.__name__):
+            dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,7 +34,7 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
+    
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -48,3 +58,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside -
+        if obj is equal to None, the method should not do anything"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__object[key]
