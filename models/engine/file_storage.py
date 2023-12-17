2fed8c2fa5255b62def091ccdd3a64523030a291
sarora2200 <sarora2200@gmail.com>
2023-12-17 13:36:12 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,7 +69,7 @@ class FileStorage:
         if obj:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
-                del self.__objects[key]
+                self.__objects.pop(key, None)
 
     def close(self):
         """
