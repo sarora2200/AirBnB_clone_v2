4b7ee47cad79809e948925439813a78c0eab54ff
sarora2200 <sarora2200@gmail.com>
2023-12-20 07:40:41 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,7 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 import os
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from importlib import import_module
 
 
 class FileStorage:
@@ -16,49 +10,60 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+    def __init__(self):
+        """Initializes a FileStorage instance"""
+        self.model_classes = {
+            'BaseModel': import_module('models.base_model').BaseModel,
+            'User': import_module('models.user').User,
+            'State': import_module('models.state').State,
+            'City': import_module('models.city').City,
+            'Amenity': import_module('models.amenity').Amenity,
+            'Place': import_module('models.place').Place,
+            'Review': import_module('models.review').Review
+        }
+
     def all(self, cls=None):
-        """Returns a dictionary of models"""
-        if not cls:
-            return FileStorage.__objects
+        """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return self.__objects
         else:
-            newdict = {}
+            filtered_dict = {}
             for key, value in self.__objects.items():
-                if isinstance(value, cls):
-                    newdict[key] = value
-            return newdict
+                if type(value) is cls:
+                    filtered_dict[key] = value
+            return filtered_dict
+
+    def delete(self, obj=None):
+        """Removes an object from the storage dictionary"""
+        if obj is not None:
+            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_key in self.__objects.keys():
+                del self.__objects[obj_key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        self.__objects.update(
+            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
+        )
 
     def save(self):
         """Saves storage dictionary to file"""
-        temp = {key: val.to_dict() for key, val
-                in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(temp, f)
+        with open(self.__file_path, 'w') as file:
+            temp = {}
+            for key, val in self.__objects.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, file)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        try:
-            with open(os.path.abspath(FileStorage.__file_path),
-                      "r", encoding="UTF8") as fd:
-                data = json.load(fd)
-                for val in data.values():
-                    class_name = val.pop("__class__", None)
-                    if class_name and class_name in globals():
-                        cls = globals()[class_name]
-                        self.new(cls(**val))
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """Delete obj"""
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects.pop(key, None)
+        classes = self.model_classes
+        if os.path.isfile(self.__file_path):
+            temp = {}
+            with open(self.__file_path, 'r') as file:
+                temp = json.load(file)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
 
     def close(self):
-        """Deserializing the JSON file to objects"""
+        """Closes the storage engine."""
         self.reload()
