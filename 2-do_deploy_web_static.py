ae3f63a84f6f39df4139bcb7ece15b9029913a34
sarora2200 <sarora2200@gmail.com>
2024-01-10 14:13:06 -0800

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,63 @@
 #!/usr/bin/python3
+
 """
-script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers
+This Fabric script deploys web static content to remote servers.
 """
 
-from fabric.api import run, put, env
+from fabric.api import env, put, run
+from os.path import exists
 import os
 
+# Define the remote hosts
 env.hosts = ['54.236.54.61', '52.207.208.2']
 
 
 def do_deploy(archive_path):
     """
-    distributes an archive to your web servers
+    This function deploys an archive to the web servers.
+
+    :param archive_path: The path to the archive to be deployed.
+    :type archive_path: str
+    :return: True if successful, False otherwise.
+    :rtype: bool
     """
-    # verificamos si el path existe
-    if os.path.exists(archive_path) is False:
-        return(False)
+    # Check if archive_path exists
+    if not exists(archive_path):
+        return False
+
+    # Get the file name without extension
+    archive_name = os.path.basename(archive_path)
+    archive_name_no_ext = os.path.splitext(archive_name)[0]
+
     try:
-        put(archive_path, '/tmp/')
-        _filename = archive_path.split("/")[-1]
-        filename = _filename.split(".")[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
-            (_filename, filename))
-        run('rm /tmp/{}'.format(_filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(filename))
-        return(True)
-    except:
-        return(False)
+        # Upload archive to the temporary folder on the web server
+        put(archive_path, "/tmp/")
+
+        # Create the directory where the code will be deployed
+        run("sudo mkdir -p /data/web_static/releases/{}/"
+            .format(archive_name_no_ext))
+
+        # Uncompress the archive into the deployment folder
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, archive_name_no_ext))
+
+        # Remove the archive from the server
+        run("sudo rm /tmp/{}".format(archive_name))
+
+        # Move the files to a new folder and delete the old symbolic link
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/"
+            .format(archive_name_no_ext, archive_name_no_ext))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_name_no_ext))
+
+        # Delete the old symbolic link and create a new one
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
+                /data/web_static/current".format(archive_name_no_ext))
+
+        return True
+
+    except Exception as e:
+        # If there is an error, return False
+        return False
